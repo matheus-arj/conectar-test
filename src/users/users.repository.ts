9a3df9c2cbd6fd5b48fe871060d55e994b150f11
matheus-arj <matheus.arj@hotmail.com>
2025-05-17 00:04:51 -0300

    feat: created user repository, method to create new user and method to verify user by email

---
+++ src/users/users.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UserRoleEnum } from './enum/user-role.enum';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  public async createUser(data: CreateUserDto): Promise<User> {
+    const user: Partial<User> = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      role: data.role || UserRoleEnum.USER,
+    };
+
+    const newUser = this.usersRepository.create(user);
+    return await this.usersRepository.save(newUser);
+  }
+
+  public async findByEmail(email: string): Promise<User | null> {
+    return await this.usersRepository.findOne({ where: { email } });
+  }
+}
