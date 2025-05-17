4569e36429ca20fe30f0c0c5d977b5818276b326
matheus-arj <matheus.arj@hotmail.com>
2025-05-17 00:03:10 -0300

    feat: created post api to create new user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  @Inject(UsersService)
+  private readonly usersService: UsersService;
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
