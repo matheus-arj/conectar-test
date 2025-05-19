aa29610ea5187929e6dd4383e403c01e7cf76dbe
matheus-arj <matheus.arj@hotmail.com>
2025-05-19 12:35:48 -0300

    feat: configure global settings by enabling ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { AppDataSource } from './data-source';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
