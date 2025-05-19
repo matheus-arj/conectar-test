0af85fdb74cffd5046195d918b4150f3e73102ff
matheus-arj <matheus.arj@hotmail.com>
2025-05-19 12:51:40 -0300

    feat: add Swagger integration for API documentation and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Conectar Test')
+    .setDescription(
+      'Documentação da API de usuários com autenticação JWT e controle de roles (ADMIN/USER).',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
